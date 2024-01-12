a223a40795005cacbbc5ba2b54195c1ca608507f
Michle99 <sammiyonyx@gmail.com>
2024-01-12 18:29:16 -0500

    renamed & updated to `HomeView` and

---
+++ src/components/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h1>Welcome to the Home Page!</h1>
+      <router-link to="/shopping-list">
+        <button>Go to Shopping List</button>
+      </router-link>
+    </div>
+</template>
+  
+<script>
+  export default {
+    // Component logic goes here
+  };
+</script>
+  
+<style scoped>
+/* Component styles go here */
+</style>
+  
