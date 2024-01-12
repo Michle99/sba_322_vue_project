275fc6322c4e921ff2a56c73edc85b2498bc5052
Michle99 <sammiyonyx@gmail.com>
2024-01-12 16:22:02 -0500

    updated `ShoppingList`

--- src/components/ShoppingList.vue
+++ src/components/ShoppingList.vue
@@ -1,16 +1,43 @@
 <template>
   <div>
-    Shopping List
+    <h2>Shopping List</h2>
+    
+    <!-- Input for adding items to the list -->
+    <div>
+      <input v-model="newItem" placeholder="Add an item" />
+      <button @click="addItem">Add</button>
+    </div>
+
+    <!-- Display the shopping list -->
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        {{ item }}
+        <button @click="removeItem(index)">Remove</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ShoppingList',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      newItem: '',
+      items: [],
+    };
+  },
+  methods: {
+    addItem() {
+      if (this.newItem.trim() !== '') {
+        this.items.push(this.newItem.trim());
+        this.newItem = '';
+      }
+    },
+    removeItem(index) {
+      this.items.splice(index, 1);
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
