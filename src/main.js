a0fceb9dca7e00355a8460ec7d64622b65b1883a
Michle99 <sammiyonyx@gmail.com>
2024-01-12 17:31:35 -0500

    updated `main.js` with imported Highlight Directive

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import highlightDirective from './directives/highlightDirective';
 
 const app = createApp(App);
 // app.use();
-app.directive('highlight')
+app.directive('highlight', highlightDirective);
 app.mount('#app');
