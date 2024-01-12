cb4b2322a8f00b5f360076984311be88da985e43
Michle99 <sammiyonyx@gmail.com>
2024-01-12 18:32:56 -0500

    updated `main.js` with router

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import highlightDirective from './directives/highlightDirective';
 import { uppercase } from './directives/uppercase';
+import router from './router/index';
 
 const app = createApp(App);
-// app.use();
+
 app.directive('highlight', highlightDirective);
 app.directive('uppercase', uppercase);
+
+app.use(router);
 app.mount('#app');
