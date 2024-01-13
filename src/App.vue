3db08c3befd728471da91a18fb057a4abacc2def
Michle99 <sammiyonyx@gmail.com>
2024-01-12 19:07:00 -0500

    updated the `router-link` style

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <router-link to="/">Home</router-link>
-    <router-link to="/shopping-list">Shopping List</router-link>
+    <router-link class="nav-link" to="/">Home</router-link>
+    <router-link class="nav-link" to="/shopping-list">Shopping List</router-link>
 
     <router-view></router-view>
 
@@ -43,5 +43,21 @@ export default {
 
 body {
   background-color: #2c3e50;
+}
+.nav-link {
+  display: inline-block;
+  margin: 10px;
+  padding: 10px;
+  text-decoration: none;
+  color: #3498db; /* Blue color */
+  font-weight: bold;
+  border: 1px solid #3498db;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+}
+
+.nav-link:hover {
+  background-color: #3498db; /* Blue color on hover */
+  color: #fff; /* White text color on hover */
 }
 </style>
